35d12f91d4771813bb083b69463f4b958340409b
Hardik <hardik11dhiman@gmail.com>
2023-06-05 17:03:13 +0530

    First commit

---
+++ src/main/java/com/example/Spring/Controllers/controller.java
@@ -0,0 +1,16 @@
+package com.example.Spring.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Hello")
+public class controller {
+
+    @GetMapping("/World")
+    public String get()
+    {
+        return "Hello WORLD OF DEVOPS";
+    }
+}
